2b6d3d23771e612400f1311a81c600c2f41a7b9d
jakubowiczish <jakub77plotnikowski@gmail.com>
2020-05-29 19:14:37 +0200

    Add basis of logging in using google api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GoogleLogin from "react-google-login";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const responseGoogle = (response) => {
+        console.log(response)
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Welcome to Master Diet
+                </p>
+            </header>
+            <h1>
+                <GoogleLogin
+                    clientId="597685077162-qu9s606hglv6k5jnk4j8faj5f1dtpdp0.apps.googleusercontent.com"
+                    buttonText="Login"
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                    cookiePolicy={'single_host_origin'}
+                />,
+            </h1>
+
+        </div>
+    );
 }
 
 export default App;
