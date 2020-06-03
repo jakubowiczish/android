d44b5ff0ebe8674ce5670df63702f7c9bfcd4f11
jakubowiczish <jakub77plotnikowski@gmail.com>
2020-06-03 23:12:49 +0200

    Update

--- src/user/oauth2/OAuth2RedirectHandler.js
+++ src/user/oauth2/OAuth2RedirectHandler.js
@@ -1,34 +1,34 @@
-import React, { Component } from 'react';
-import { ACCESS_TOKEN } from '../../constants';
-import { Redirect } from 'react-router-dom'
+import React, {Component} from 'react';
+import {ACCESS_TOKEN} from '../../constants';
+import {Redirect} from 'react-router-dom'
 
 class OAuth2RedirectHandler extends Component {
     getUrlParameter(name) {
         name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-        var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+        const regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
 
-        var results = regex.exec(this.props.location.search);
+        const results = regex.exec(this.props.location.search);
         return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
     };
 
-    render() {        
+    render() {
         const token = this.getUrlParameter('token');
         const error = this.getUrlParameter('error');
 
-        if(token) {
+        if (token) {
             localStorage.setItem(ACCESS_TOKEN, token);
             return <Redirect to={{
                 pathname: "/profile",
-                state: { from: this.props.location }
-            }}/>; 
+                state: {from: this.props.location}
+            }}/>;
         } else {
             return <Redirect to={{
                 pathname: "/login",
-                state: { 
+                state: {
                     from: this.props.location,
-                    error: error 
+                    error: error
                 }
-            }}/>; 
+            }}/>;
         }
     }
 }
