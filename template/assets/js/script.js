7693432aab8252a96732e9043a74d67194d938a1
Rafał Lisak <rafal.lisak1@gmail.com>
2020-06-17 17:21:20 +0200

    [MASTERDIET-17] responsive template of website added

---
+++ template/assets/js/script.js
@@ -0,0 +1,7 @@
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    $("#mainNav").toggleClass("mobileNav")
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+});
