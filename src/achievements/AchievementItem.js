59c455ad04ac993014cc0abe91a5662d3f1c1ec2
Rafał Lisak <rafal.lisak1@gmail.com>
2020-07-26 19:22:54 +0200

    [MASTERDIET-25] AchievementItem.js added

--- src/achievements/AchievementItem.js
+++ src/achievements/AchievementItem.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import './Achievement.css'
-
+import ProgressBar from 'react-bootstrap/ProgressBar'
 class AchievementItem extends Component {
   render () {
     return (
-      <div className='product-item'>
+      <div className={"achievement-div"}>
         <b>{this.props.name}</b>
         <b>{this.props.description}</b>
         <b>{this.props.points}</b>
@@ -12,6 +12,7 @@ class AchievementItem extends Component {
         <b>{this.props.photo}</b>
         <b>{this.props.progress}</b>
         <b>{this.props.completedDate}</b>
+        <ProgressBar animated now={this.props.progress}  label={`${this.props.progress}%`}/>
       </div>
     )
   }
--- src/achievements/AchievementsList.js
+++ src/achievements/AchievementsList.js
@@ -7,37 +7,41 @@ class AchievementsList extends Component {
   constructor (props) {
     super(props)
     this.getAchievements();
+    this.state = {
+      achievements: [],
+    }
   }
   getAchievements () {
     getUserAchievements().then(
       response => {
         this.setState({
           achievements: response,
-        })
+        });
+        console.log('ASD');
+        console.log(response);
       }
     )
   }
-
   render () {
-    const userAchievements = this.state.achievements.map(achievement =>
-      <AchievementItem
-        name={achievement.name}
-        description={achievement.description}
-        points={achievement.points}
-        completeCondition={achievement.completeCondition}
-        photo={achievement.photo}
-        progress={achievement.progress}
-        completedDate={achievement.completedDate}
-      />)
 
     return (
       <div>
-      <div>ASADSdasasdasd</div>
-      <div>
-        <List>
-          {userAchievements}
-        </List>
-      </div>
+        <div>ASADSdasasdasd</div>
+        <div>
+          <List>
+            {this.state.achievements.map(achievement =>
+              <AchievementItem
+                key = {achievement.name}
+                name={achievement.name}
+                description={achievement.description}
+                points={achievement.points}
+                completeCondition={achievement.completeCondition}
+                photo={achievement.photo}
+                progress={achievement.progress}
+                completedDate={achievement.completedDate}
+              />)}
+          </List>
+        </div>
       </div>
     )
   }
