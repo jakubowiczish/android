b741490ae1259713ee4c98b4aa049135444de1e6
Jakub Płotnikowski <jakub77plotnikowski@gmail.com>
2020-10-22 17:54:16 +0200

    [MASTERDIET-60] Update containers for logging in

--- src/user/login/Login.js
+++ src/user/login/Login.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react'
 import './Login.css'
-import { ACCESS_TOKEN, FACEBOOK_AUTH_URL, GITHUB_AUTH_URL, GOOGLE_AUTH_URL } from '../../constants'
-import { login } from '../../util/APIUtils'
 import { Redirect } from 'react-router-dom'
-import fbLogo from '../../img/social-icons/facebook-logo.png'
-import googleLogo from '../../img/google-logo.png'
-import githubLogo from '../../img/social-icons/github-logo.png'
 import Alert from 'react-s-alert'
 import LoginSignupContainer from './LoginSignupContainer'
 
 class Login extends Component {
+
   componentDidMount () {
+
     // If the OAuth2 login encounters an error, the user is redirected to the /login page with an error.
     // Here we display the error and then remove the error query parameter from the location.
+
     if (this.props.location.state && this.props.location.state.error) {
       setTimeout(() => {
         Alert.error(this.props.location.state.error, {
@@ -63,67 +61,6 @@ class Login extends Component {
           </div>
         </div>
       </div>
-    )
-  }
-}
-
-class LoginForm extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      email: '',
-      password: ''
-    }
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  handleInputChange (event) {
-    const target = event.target
-    const inputName = target.name
-    const inputValue = target.value
-
-    this.setState({
-      [inputName]: inputValue
-    })
-  }
-
-  handleSubmit (event) {
-    event.preventDefault()
-
-    const loginRequest = Object.assign({}, this.state)
-    login(loginRequest)
-      .then(response => {
-        localStorage.setItem(ACCESS_TOKEN, response.accessToken)
-        this.props.history.push('/')
-        window.location.reload(true)
-        Alert.success('You\'re successfully logged in!')
-      }).catch(error => {
-      Alert.error((error && error.message) || 'Oops! Something went wrong. Please try again!')
-    })
-  }
-
-  render () {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div className='form-item'>
-          <input
-            type='email' name='email'
-            className='form-control' placeholder='Email'
-            value={this.state.email} onChange={this.handleInputChange} required
-          />
-        </div>
-        <div className='form-item'>
-          <input
-            type='password' name='password'
-            className='form-control' placeholder='Password'
-            value={this.state.password} onChange={this.handleInputChange} required
-          />
-        </div>
-        <div className='form-item'>
-          <button type='submit' className='btn btn-block btn-primary'>Login</button>
-        </div>
-      </form>
     )
   }
 }
--- src/user/login/LoginSignupContainer.js
+++ src/user/login/LoginSignupContainer.js
@@ -24,7 +24,7 @@ class LoginSignupContainer extends React.Component {
   componentDidMount () {
     const signUpButton = document.getElementById('signUp')
     const signInButton = document.getElementById('signIn')
-    const container = document.getElementById('container')
+    const container = document.getElementById('login_signup_container_id')
 
     signUpButton.addEventListener('click', () => {
       container.classList.add('right-panel-active')
@@ -87,77 +87,72 @@ class LoginSignupContainer extends React.Component {
 
   render () {
     return (
-      <div>
-        <div className="center-login-signup-form">
-          <div className="login-signup-container" id="container">
-            <div className="form-container sign-up-container">
-              <form action="#" onSubmit={this.handleSignupSubmit}>
-                <h1>Create Account</h1>
-                <div className="social-container">
-                  <a href={FACEBOOK_AUTH_URL} className="social fab facebook-icon"/>
-                  <a href={GOOGLE_AUTH_URL} className="social fab google-icon"/>
-                  <a href={GITHUB_AUTH_URL} className="social fab github-icon"/>
-                </div>
-                <span>or use your email for registration</span>
-                <input type="text"
-                       placeholder="Name"
-                       name="signupName"
-                       value={this.state.signupName}
-                       onChange={this.handleInputChange} required
-                />
-                <input type="email"
-                       placeholder="Email"
-                       name="signupEmail"
-                       value={this.state.signupEmail}
-                       onChange={this.handleInputChange} required
-                />
-                <input type="password"
-                       placeholder="Password"
-                       name="signupPassword"
-                       value={this.state.signupPassword}
-                       onChange={this.handleInputChange} required
-                />
-                <button>Sign Up</button>
-              </form>
+      <div className="login-signup-container" id="login_signup_container_id">
+        <div className="form-container sign-up-container">
+          <form action="#" onSubmit={this.handleSignupSubmit}>
+            <h1>Create Account</h1>
+            <div className="social-container">
+              <a href={FACEBOOK_AUTH_URL} className="social fab facebook-icon"/>
+              <a href={GOOGLE_AUTH_URL} className="social fab google-icon"/>
+              <a href={GITHUB_AUTH_URL} className="social fab github-icon"/>
             </div>
-            <div className="form-container sign-in-container">
-              <form action="#" onSubmit={this.handleLoginSubmit}>
-                <h1>Sign in</h1>
-                <div className="social-container">
-                  <a href={FACEBOOK_AUTH_URL} className="social fab facebook-icon"/>
-                  <a href={GOOGLE_AUTH_URL} className="social fab google-icon"/>
-                  <a href={GITHUB_AUTH_URL} className="social fab github-icon"/>
-                </div>
-                <span>or use your account</span>
-                <input type="email"
-                       placeholder="Email"
-                       name="loginEmail"
-                       value={this.state.loginEmail}
-                       onChange={this.handleInputChange} required
-                />
-                <input type="password"
-                       placeholder="Password"
-                       name="loginPassword"
-                       value={this.state.loginPassword}
-                       onChange={this.handleInputChange} required
-                />
-                <a href="#">Forgot your password?</a>
-                <button>Sign In</button>
-              </form>
+            <span>or use your email for registration</span>
+            <input type="text"
+                   placeholder="Name"
+                   name="signupName"
+                   value={this.state.signupName}
+                   onChange={this.handleInputChange} required
+            />
+            <input type="email"
+                   placeholder="Email"
+                   name="signupEmail"
+                   value={this.state.signupEmail}
+                   onChange={this.handleInputChange} required
+            />
+            <input type="password"
+                   placeholder="Password"
+                   name="signupPassword"
+                   value={this.state.signupPassword}
+                   onChange={this.handleInputChange} required
+            />
+            <button>Sign Up</button>
+          </form>
+        </div>
+        <div className="form-container sign-in-container">
+          <form action="#" onSubmit={this.handleLoginSubmit}>
+            <h1>Sign in</h1>
+            <div className="social-container">
+              <a href={FACEBOOK_AUTH_URL} className="social fab facebook-icon"/>
+              <a href={GOOGLE_AUTH_URL} className="social fab google-icon"/>
+              <a href={GITHUB_AUTH_URL} className="social fab github-icon"/>
             </div>
-            <div className="overlay-container">
-              <div className="overlay">
-                <div className="overlay-panel overlay-left">
-                  <h1>Welcome Back!</h1>
-                  <p>To keep connected with us please login with your personal info</p>
-                  <button className="ghost" id="signIn">Sign In</button>
-                </div>
-                <div className="overlay-panel overlay-right">
-                  <h1>Hello, Friend!</h1>
-                  <p>Please enter your personal details and start your journey</p>
-                  <button className="ghost" id="signUp">Sign Up</button>
-                </div>
-              </div>
+            <span>or use your account</span>
+            <input type="email"
+                   placeholder="Email"
+                   name="loginEmail"
+                   value={this.state.loginEmail}
+                   onChange={this.handleInputChange} required
+            />
+            <input type="password"
+                   placeholder="Password"
+                   name="loginPassword"
+                   value={this.state.loginPassword}
+                   onChange={this.handleInputChange} required
+            />
+            <button>Sign In</button>
+          </form>
+        </div>
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
+              <h1>Welcome Back!</h1>
+              <p>To keep connected with us please login with your personal info</p>
+              <button className="ghost" id="signIn">Sign In</button>
+            </div>
+            <div className="overlay-panel overlay-right">
+              <h1>Hello, Friend!</h1>
+              <p>Please enter your personal details and start your journey</p>
+              <button className="ghost" id="signUp">Sign Up</button>
             </div>
           </div>
         </div>
