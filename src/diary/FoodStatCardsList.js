2b7ccb6da312eae6a74f16454c9febbf572e7eee
Rafał Lisak <rafal.lisak1@gmail.com>
2020-11-16 21:47:52 +0100

    Nice example

--- src/diary/FoodStatCardsList.js
+++ src/diary/FoodStatCardsList.js
@@ -5,22 +5,17 @@ import './FoodStatCardsList.css'
 import FoodStatCard from './FoodStatCard'
 
 class FoodStatCardsList extends React.Component {
-
-  mon = () => {
-    console.log(this.props.summaryList)
-  }
   render () {
     return (
       <div>
-      <button onClick={this.mon}>ASDADSASD</button>
-      <List className='main'>
-        {this.props.summaryList && this.props.summaryList.map(stat =>
-          <FoodStatCard
-            description={stat.description}
-            difference={stat.difference}
-            sum={stat.sum}
-          />)}
-      </List>
+        <List className='main'>
+          {this.props.summaryList && this.props.summaryList.map(stat =>
+            <FoodStatCard
+              description={stat.description}
+              difference={stat.difference}
+              sum={stat.sum}
+            />)}
+        </List>
       </div>
     )
   }
